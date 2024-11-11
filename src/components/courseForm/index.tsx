44c42c2065aa75acb155a9f6ce104112d18fa879
Ihshan <ihshanaushad2732@gmail.com>
2024-11-11 14:30:01 +0530

    Student Form Changes

--- src/components/courseForm/index.tsx
+++ src/components/courseForm/index.tsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from 'react';
 
 interface Props {
@@ -37,32 +39,62 @@ const CourseForm: React.FC<Props> = ({ onSubmit }) => {
   return (
     <div>
       {alertMessage && <div className="alert">{alertMessage}</div>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Course Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        
+        <div>
+        <label>
+          Course Name
+          <input
+            type="text"
+            placeholder="Course Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ width: '100%', padding: '0.5rem', borderRadius: '10px', border: '1px solid #ccc' }}
+            />
+        </label>
+        </div>
+        <div>
+        <label>
+        Start Date
         <input
           type="datetime-local"
           placeholder="Start Date"
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
-        />
+          style={{ width: '100%', padding: '0.5rem', borderRadius: '10px', border: '1px solid #ccc' }}
+          />
+        </label>
+        </div>
+
+        <div>
+        <label> End Date
         <input
           type="datetime-local"
           placeholder="End Date"
           value={endDate}
           onChange={(e) => setEndDate(e.target.value)}
-        />
+          style={{ width: '100%', padding: '0.5rem', borderRadius: '10px', border: '1px solid #ccc' }}
+          />
+        </label>
+        </div>
+
+        <div>
+        <label>Batch No
         <input
           type="text"
           placeholder="Batch No"
           value={batchNo}
           onChange={(e) => setBatchNo(e.target.value)}
-        />
-        <button type="submit">Create Course</button>
+          style={{ width: '100%', padding: '0.5rem', borderRadius: '10px', border: '1px solid #ccc' }}
+          />
+        </label>
+        </div>
+
+        <div>
+        <button type="submit"
+        style={{ width: '100%', padding: '0.5rem', borderRadius: '10px', border: '1px solid #ccc', backgroundColor:'Blue' }}
+        >Create Course</button>
+        </div>
       </form>
     </div>
   );
